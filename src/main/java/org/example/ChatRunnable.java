2c88b71859d9d7ce5fc68fdab0385708b3de476b
patton64 <patton64@pnw.edu>
2025-02-21 08:41:56 -0600

    Letting the client end without a really large error message.

--- src/main/java/org/example/ChatRunnable.java
+++ src/main/java/org/example/ChatRunnable.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class ChatRunnable implements Runnable {
 
@@ -25,7 +26,11 @@ public class ChatRunnable implements Runnable {
             while((message = in.readLine()) != null){
                 System.out.println(message);
 
-            }        } catch (IOException e) {
+            }
+        }catch (SocketException e){
+            logger.error("Client disconnected: {}", e.getMessage());
+            System.out.println("Cya nerd\uD83D\uDC80");
+        } catch (IOException e) {
             logger.error("Error reading message: {}", e.getMessage(), e);
         }
     }
